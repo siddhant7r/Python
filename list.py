4c5b071d3e6a816f578a35f816f0525f0cfa95fb
Siddhant <siddhant11raj@gmail.com>
2024-06-24 14:45:39 +0530

    3rd commit

---
+++ list.py
@@ -0,0 +1,36 @@
+my_list=[10,20,30,"sid","vini","shreya",100]
+
+# print(my_list)
+# print(my_list[0])
+# # print(my_list[-1])
+# print(my_list[6])
+# print(my_list[2:])
+
+my_list[0]="shubham"   #replace the value which is present to the new name(shubham)
+print(my_list)
+
+#inbuilt functions
+
+my_list1=[28,2,34,54,65,90]
+
+# print(len(my_list1))
+# print(max(my_list1))
+# print(min(my_list1))
+# print(sum(my_list1))
+
+my_list1.append(50)
+print(my_list1)
+
+# my_list.extend(my_list1)
+# print(my_list)
+
+my_list.pop()
+
+my_list.remove(20)
+
+my_list.insert(3,"Radhika")
+
+my_list1.sort()
+
+my_list1.sort(reverse=True)
+print(my_list1)
